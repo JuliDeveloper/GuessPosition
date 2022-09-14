6fceb002a1625cb21cee6e4e49957e9c444bedad
JuliDeveloper <rja-97@yandex.ru>
2022-09-14 22:15:19 +0300

    Исправила код

--- GuessPosition/ContentView.swift
+++ GuessPosition/ContentView.swift
@@ -9,27 +9,36 @@ import SwiftUI
 
 struct ContentView: View {
     
-    @State var targetValue: Int = 40
-    @State var currentValue: Float = 0.0
+    @State var targetValue: Int = Int.random(in: 1...100)
+    @State var currentValue: Double = 40.0
     @State var isPresentedAlert: Bool = false
     
     var body: some View {
         VStack(spacing: 50) {
             TextCheckNumber(value: $targetValue)
-            SliderView(sliderValue: $currentValue)
+            
+            SliderView(sliderValue: $currentValue,
+                       color: .orange,
+                       opacity: computeScore())
+            
             VStack(spacing: 20) {
-                ButtonView(title: "Проверь меня!", action: { isPresentedAlert.toggle() })
+                
+                ButtonView(title: "Проверь меня!",
+                           action: { isPresentedAlert.toggle() })
                     .alert("Твой счёт:", isPresented: $isPresentedAlert, actions: {}) {
                         Text("\(computeScore())")
                     }
-                ButtonView(title: "Начать заново", action: { targetValue = Int.random(in: 0...100) })
+                
+                ButtonView(title: "Начать заново") {
+                    targetValue = Int.random(in: 0...100)
+                }
             }
         }
         .padding()
     }
     
     private func computeScore() -> Int {
-        let difference = abs(targetValue - lround(Double(currentValue)))
+        let difference = abs(targetValue - lround(currentValue))
         return 100 - difference
     }
 }
