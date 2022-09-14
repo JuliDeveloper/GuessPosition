434407f69ea3889d04baabc70ba4bfbf279c84cd
JuliDeveloper <rja-97@yandex.ru>
2022-09-14 15:46:54 +0300

    Initial Commit

---
+++ GuessPosition/GuessPositionApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessPositionApp.swift
+//  GuessPosition
+//
+//  Created by Julia Romanenko on 14.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct GuessPositionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
