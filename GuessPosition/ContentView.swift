d91265b6af17fcbea7bfc2e1bf747a4b7b3e3009
JuliDeveloper <rja-97@yandex.ru>
2022-09-14 16:37:06 +0300

    Добавила текст и две кнопки на ContentView

--- GuessPosition/ContentView.swift
+++ GuessPosition/ContentView.swift
@@ -8,9 +8,23 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var value: Int = 30
+    @State var isPresentedAlert: Bool = false
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack(spacing: 20) {
+            TextCheckNumber(value: $value)
+            ButtonView(title: "Проверь меня!", action: checkValue)
+                .alert("Твой счёт:", isPresented: $isPresentedAlert, actions: {}) {
+                    Text("")
+                }
+            ButtonView(title: "Начать заново", action: { value = 0 })
+        }
+    }
+    
+    private func checkValue() {
+        isPresentedAlert.toggle()
     }
 }
 
